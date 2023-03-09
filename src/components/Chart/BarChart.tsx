e838016af3081dab8350613a38eaca91fd94f04b
czhen <56986964+shczhen@users.noreply.github.com>
2023-03-09 14:58:31 +0800

    Update BarChart.tsx

--- src/components/Chart/BarChart.tsx
+++ src/components/Chart/BarChart.tsx
@@ -18,30 +18,68 @@ export interface BarChartProps {
   data: { [key: string]: string | number | undefined }[];
   xDataKey: string;
   barDataKey: string;
+  width?: number;
+  height?: number;
+  margin?: {
+    top?: number;
+    right?: number;
+    left?: number;
+    bottom?: number;
+  };
+  disableCartesianGrid?: boolean;
+  cartesianGridProps?: {
+    stroke?: string;
+  };
+  xAxisProps?: {
+    tick?: any;
+    height?: number;
+    interval?: number;
+  };
+  yAxisProps?: {
+    tickFormatter?: (value: number) => string;
+  };
+  tooltipProps?: {};
 }
 
 export default function BarChartComponent(props: BarChartProps) {
-  const { data, xDataKey, barDataKey } = props;
+  const {
+    data,
+    xDataKey,
+    barDataKey,
+    width,
+    height,
+    margin,
+    disableCartesianGrid,
+    cartesianGridProps,
+    xAxisProps,
+    yAxisProps,
+    tooltipProps,
+    legendProps,
+  } = props;
 
   return (
     <ResponsiveContainer width="100%" height="100%">
       <BarChart
-        width={500}
-        height={300}
+        width={width || 500}
+        height={height || 300}
         data={data}
         margin={{
           top: 5,
           right: 30,
           // left: 20,
           bottom: 5,
+          ...margin,
         }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
+        {!disableCartesianGrid && (
+          <CartesianGrid strokeDasharray="3 3" {...cartesianGridProps} />
+        )}
         <XAxis
           dataKey={xDataKey}
           tick={<CustomizedAxisTick />}
           height={120}
           interval={0}
+          {...xAxisProps}
         />
         <YAxis
           // scale="log"
@@ -58,8 +96,9 @@ export default function BarChartComponent(props: BarChartProps) {
           // type="number"
           tickFormatter={(value) => `${nFormatter(value, 1)}`}
           dataKey={barDataKey}
+          {...yAxisProps}
         />
-        <Tooltip />
+        <Tooltip {...tooltipProps} />
         <Bar dataKey={barDataKey} fill={stringToColor(barDataKey)} />
       </BarChart>
     </ResponsiveContainer>
